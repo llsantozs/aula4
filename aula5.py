8af32f8396cccd9367ba08520f3de968f88d3b5e
laura <ls4824218@gmail.com>
2024-05-15 15:24:13 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,59 @@
+
+# Exercícios: 
+
+# 1 Imprima uma mensagem de boas-vindas na tela.
+
+print('Sejam bem-vindos!')
+
+
+# 2 Declare uma variável booleana verdadeiro
+
+booleana =  True
+print(type(booleana))
+
+# com o valor True e imprima seu tipo
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+print(1.9 * 9.9)
+
+# 4 Imprima o resultado da divisão de dois números inteiros 
+
+print(90/5)
+
+# de sua escolha.
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+
+print(10-2)
+
+# 6 Imprima o resultado da divisão inteira de dois números inteiros 
+
+print(50//9)
+# de sua escolha.
+# 7 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+print(20*9)
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, 
+# imprima o dobro desse número
+
+valor = 10
+print(valor *2)
+
+# 9 Crie uma calculadora de soma com as ferramentas que vc já 
+# conhece(Use apenas input, print e variavel)
+
+# n1 = float(input('Digite um numero'))
+# n2 = float(input('Digite outro numero'))
+# print(f'''
+# Soma = {n1 + n2}
+# Subtração = {n1 - n2}
+# Multiplicacao = {n1 * n2}
+# Divisao = {n1 // n2}''') 
+
+# 10 Crie um sistema de cadastro com as estruturas que vc já 
+# conhece(Use apenas input, print e variavel)
+
+nome = input('Digite seu nome: ')
+email = input('Digite seu email: ')
+idade = input('Digite sua idade: ')
+altura = input('Digite sua altura: ')
